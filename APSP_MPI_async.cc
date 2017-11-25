b0ea60f9f43c415cda1ce452b849dfe3f27c1ea6
Ending2015a <joe85812@yahoo.com.tw>
2017-11-25 19:43:09 +0800

    123413251

--- APSP_MPI_async.cc
+++ APSP_MPI_async.cc
@@ -219,11 +219,11 @@ inline void dump_to_file(const char *file){
 
 
 inline void create_graph(){
-    //MPI_Dist_graph_create(MPI_COMM_WORLD, 1, &world_rank, &neighbor_count, 
-    //        neighbor_list.data(), MPI_UNWEIGHTED, MPI_INFO_NULL, false, &COMM_GRAPH);
-    //MPI_Comm_rank(COMM_GRAPH, &graph_rank);
-    COMM_GRAPH = MPI_COMM_WORLD;
-    graph_rank = world_rank;
+    MPI_Dist_graph_create(MPI_COMM_WORLD, 1, &world_rank, &neighbor_count, 
+            neighbor_list.data(), MPI_UNWEIGHTED, MPI_INFO_NULL, false, &COMM_GRAPH);
+    MPI_Comm_rank(COMM_GRAPH, &graph_rank);
+    //COMM_GRAPH = MPI_COMM_WORLD;
+    //graph_rank = world_rank;
 }
 
 inline int check_all_no_update(){
